0b32ea4edcb5f338b14572ee3b6471ae1349f812
Lokotochek <lokotochek@gmail.com>
2014-12-04 01:13:52 +0300

    hash version 1

---
+++ Documents/proga/hashtable/hashtable/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  hashtable
+//
+//  Created by Lokotochek on 08.11.14.
+//  Copyright (c) 2014 Alexander Laktionov. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
