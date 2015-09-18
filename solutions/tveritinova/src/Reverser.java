f46a7fcb90c7e940d9afd56fd90ce51cecee956b
Tveritinova <jane281096@gmail.com>
2015-09-19 00:25:27 +0300

    Change style

--- solutions/tveritinova/src/Reverser.java
+++ solutions/tveritinova/src/Reverser.java
@@ -1,14 +1,13 @@
 public class Reverser {
-    public static void main(String[] args)
-    {
-        for (int i = args.length - 1; i >= 0; i--)
         {
+    public static void main(String[] args) {
+        for (int i = args.length - 1; i >= 0; i--) {
             String[] parts = args[i].split("\\s+");
-            for (int j = parts.length - 1; j >= 0; j--)
-            {
+            for (int j = parts.length - 1; j >= 0; j--) {
                 if (!parts[j].equals(""))
-                    System.out.println(parts[j]);
+                    System.out.print(parts[j] + " ");
             }
         }
+        System.out.println();
     }
 }
