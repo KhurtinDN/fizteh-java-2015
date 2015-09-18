bb21646cc9a2ecaa31da0f77a3cef5364ba6b619
Semyon Fedotov <simon23rus@gmail.com>
2015-09-19 00:12:10 +0300

    Oooops

--- solutions/simon23rus/Main.java
+++ solutions/simon23rus/Main.java
@@ -3,7 +3,7 @@ public class Main {
     public static void main(String[] args) {
         for(int pos = args.length - 1; pos >= 0; --pos) {
             String[] thisString = args[pos].split("\\s");
-            for(int toPrint = 0; toPrint < thisString.length; ++toPrint) {
+            for(int toPrint = thisString.length -1; toPrint >= 0; --toPrint) {
                 System.out.print(thisString[toPrint] + " ");
             }
         }
