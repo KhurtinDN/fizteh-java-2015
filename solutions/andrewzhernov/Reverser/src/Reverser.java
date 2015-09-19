d4daaa788b0cae2c2f364d5028a55c3c6ff75c90
Andrew Zhernov <andrewzhernov@yandex.ru>
2015-09-19 21:31:28 +0300

    a string's trim was added

--- solutions/andrewzhernov/Reverser/src/Reverser.java
+++ solutions/andrewzhernov/Reverser/src/Reverser.java
@@ -1,5 +1,6 @@
 public class Reverser {
     public static void main(String[] args) {
-        System.out.println(new StringBuffer(String.join(" ", args)).reverse().toString().replaceAll("\\s+", " "));
+        System.out.println(new StringBuffer(String.join(" ", args))
+                .reverse().toString().replaceAll("\\s+", " ").trim());
     }  
 }
