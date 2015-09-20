038f3e8cd5fdb523d7faf711fd5ccfa378d5439d
Tveritinova <jane281096@gmail.com>
2015-09-20 23:52:42 +0300

    Reverser

--- solutions/tveritinova/src/Reverser.java
+++ solutions/tveritinova/src/Reverser.java
@@ -1,5 +1,4 @@
 public class Reverser {
-        {
     public static void main(String[] args) {
         for (int i = args.length - 1; i >= 0; i--) {
             String[] parts = args[i].split("\\s+");
