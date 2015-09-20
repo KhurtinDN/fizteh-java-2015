df0cf7ec961ca14e01c207980da12786f5f7b405
Alex <aleksey.zotov@phystech.edu>
2015-09-21 01:44:36 +0300

    start twitterstream task

---
+++ projects/ale3otik/src/main/java/ru/mipt/diht/students/ale3otik/Twitter/TwitterStream.java
@@ -0,0 +1,10 @@
+package ru.mipt.diht.students.ale3otik.Twitter;
+
+/**
+ * Created by alex on 21.09.15.
+ */
+public class TwitterStream {
+    public static void main(String[] args){
+        
+    }
+}
