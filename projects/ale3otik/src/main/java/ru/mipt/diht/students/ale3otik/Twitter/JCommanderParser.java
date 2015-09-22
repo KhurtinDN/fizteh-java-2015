734e44632eb2068dfce434149a234fecb80e41c8
Alex <aleksey.zotov@phystech.edu>
2015-09-23 02:49:11 +0300

    Add JCommander to parse arguments

---
+++ projects/ale3otik/src/main/java/ru/mipt/diht/students/ale3otik/Twitter/JCommanderParser.java
@@ -0,0 +1,42 @@
+package ru.mipt.diht.students.ale3otik.Twitter;
+
+/**
+ * Created by alex on 23.09.15.
+ */
+
+
+import com.beust.jcommander.Parameter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class JCommanderParser {
+
+    @Parameter
+    private List<String> parameters = new ArrayList<>();
+
+    @Parameter(names = {"--stream", "-s"},
+            description = "print stream of new tweets")
+    private boolean stream = false;
+
+    @Parameter(names = {"--help", "-h"}, description = "print help man")
+    private boolean help = false;
+
+    @Parameter(names = {"--query", "-q"}, description = "set query parameters")
+    private String query = "";
+
+    /*@Parameter(names = {"--place", "-p"}, description = "set location")
+    private boolean help = false;*/
+
+    public boolean isStream() {
+        return stream;
+    }
+
+    public boolean isHelp() {
+        return help;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+}
--- projects/ale3otik/src/main/java/ru/mipt/diht/students/ale3otik/Twitter/TwitterStream.java
+++ projects/ale3otik/src/main/java/ru/mipt/diht/students/ale3otik/Twitter/TwitterStream.java
@@ -5,6 +5,7 @@
 
 package ru.mipt.diht.students.ale3otik.Twitter;
 
+import com.beust.jcommander.JCommander;
 import twitter4j.*;
 import twitter4j.StatusListener;
 
@@ -14,18 +15,8 @@ public class TwitterStream {
     static final String ANSI_RESET = "\u001B[0m";
     static final String ANSI_BLUE = "\u001B[34m";
 
-    public static void main(String[] args) throws TwitterException {
+    private static void streamStart(FilterQuery query) {
 
-        Twitter twitter = TwitterFactory.getSingleton();
-/*
-        Query query = new Query("AZAZZAZ");
-        QueryResult result = twitter.search(query);
-        for (Status status : result.getTweets()) {
-            System.out.println(ANSI_BLUE + "@"
-                    + status.getUser().getScreenName()
-                    + ANSI_RESET + ":  " + status.getText() + "\n");
-        }
-*/
         twitter4j.TwitterStream twStream = twitter4j
                 .TwitterStreamFactory.getSingleton();
 
@@ -37,7 +28,7 @@ public class TwitterStream {
                 System.out
                         .println(ANSI_BLUE
                                 + status.getUser()
-                                        .getScreenName()
+                                .getScreenName()
                                 + ANSI_RESET
                                 + " : " + status.getText());
             }
@@ -67,7 +58,23 @@ public class TwitterStream {
         };
 
         twStream.addListener(listener);
-        twStream.filter(new FilterQuery("Moscow"));
+        twStream.filter(query);
 
+    }
+
+    public static void main(String[] args) throws TwitterException {
+        System.out.println("\n\nTwitter 0.1 welcome\n\n");
+        JCommanderParser jcp = new JCommanderParser();
+        JCommander jcm = new JCommander(jcp, args);
+        jcm.setProgramName("TwitterStream");
+        String query = jcp.getQuery();
+
+        if (jcp.isStream()) {
+            System.out.println("\nStart stream >>> ");
+            streamStart(new FilterQuery(query));
+        }
+        if (jcp.isHelp()) {
+            System.out.println("Help");
+        }
     }
 }
