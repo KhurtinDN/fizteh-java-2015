d6f19f1d97c67b0304f906f262a40f32f2ec4ff8
sopilnyak <o.sopilniak@gmail.com>
2015-09-24 21:07:42 +0300

    Update TwitterStream

--- projects/sopilnyak/src/main/java/ru/mipt/diht/students/sopilnyak/TwitterStream/App.java
+++ projects/sopilnyak/src/main/java/ru/mipt/diht/students/sopilnyak/TwitterStream/App.java
@@ -1,5 +1,4 @@
 /*
-TODO --place - искать по IP (nearby уже определено) и по месту
 TODO: обработка Ctrl+Break (Ctrl+D) и ESC
 */
 
@@ -8,6 +7,8 @@ package ru.sopilnyak;
 import twitter4j.*;
 
 import java.io.*;
+import java.net.Inet4Address;
+import java.net.UnknownHostException;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
@@ -27,6 +28,8 @@ public class App {
     public static final String BLUE = "\u001B[34m";
     public static final String RESET = "\u001B[0m";
     public static final String HELP = "help.txt";
+    public static final int MAX_TWEETS = 100;
+    public static final int RADIUS = 10;
 
     public static void main(String[] args) {
 
@@ -91,12 +94,18 @@ public class App {
             }
         }
 
-        if (queryString != null) {
-            if (queryString.equals("")) {
-                System.err.println("No query, nothing to find");
-            } else {
-                addQuery();
-            }
+        if (queryString != null && !queryString.equals("")) { // remove space in the end
+            queryString = queryString.substring(0, queryString.length() - 1);
+        }
+
+        if (locationString != null && !locationString.equals("")) { // remove space in the end
+            locationString = locationString.substring(0, locationString.length() - 1);
+        }
+
+        if ((queryString == null || queryString.equals("")) && locationString == null) {
+            System.err.println("No query, nothing to find");
+        } else {
+            addQuery();
         }
 
     }
@@ -113,76 +122,114 @@ public class App {
     }
 
     protected static void addQuery() {
-        System.out.print("Твиты по запросу \"" + queryString + "\"");
-        if (isStreamEnabled) {
-            System.out.print(" в режиме потока");
-        }
-        if (locationString != null && !locationString.equals("") && !isNearbyEnabled) {
-            System.out.print(" возле местоположения \"" + locationString + "\"");
-        }
-        if (locationString != null && (isNearbyEnabled || locationString.equals(""))) {
-            System.out.print(" возле вашего местоположения");
-        }
-        System.out.println(":");
 
         Twitter twitter = TwitterFactory.getSingleton();
         Query query = new Query(queryString);
+        GeoQuery geoQuery;
+        try {
+            String ip = Inet4Address.getLocalHost().getHostAddress();
+            geoQuery = new GeoQuery(ip);
 
-        int attempts = 0;
-        final int MAX_ATTEMPTS = 20;
+            query.setCount(MAX_TWEETS); // max number of tweets
+            if (limit != -1) {
+                query.setCount(limit);
+            }
 
-        // print the results of the query
-        while (true) {
-            try {
-                QueryResult result = twitter.search(query); // send a query
-                int i = 0; // count tweets
-                for (Status status : result.getTweets()) { // print tweets
-                    if (i == limit) { // limit == -1 -> never break
-                        break;
-                    }
-                    if (!status.isRetweet() || !hideRetweets) { // if necessary, hide retweets
-                        if (!isStreamEnabled) {
-                            System.out.print("[" + getDate(status.getCreatedAt()) + "] ");
+            System.out.print("Твиты");
+            if (queryString != null && !queryString.equals("")) {
+                System.out.print(" по запросу \"" + queryString + "\"");
+            }
+            if (isStreamEnabled) {
+                System.out.print(" в режиме потока");
+            }
+            if (locationString != null && !locationString.equals("") && !isNearbyEnabled) {
+                System.out.print(" возле местоположения \"" + locationString + "\"");
+                geoQuery.setQuery(locationString);
+            }
+            if (locationString != null && (isNearbyEnabled || locationString.equals(""))) {
+                System.out.print(" возле вашего местоположения");
+                isNearbyEnabled = true;
+            } else {
+                isNearbyEnabled = false;
+            }
+            System.out.println(":");
+
+
+            int attempts = 0;
+            final int MAX_ATTEMPTS = 20;
+
+            // print the results of the query
+            while (true) {
+
+                try {
+
+                    if (locationString != null) {
+                        ResponseList<Place> places = twitter.searchPlaces(geoQuery);
+
+                        if (places.size() == 0) {
+                            System.out.println("Невозможно определить местоположение");
+                            return;
+
+                        } else {
+                            Place place = places.get(0); // get first place
+                            // search in radius by coordinates
+                            query.setGeoCode(place.getBoundingBoxCoordinates()[0][0], RADIUS, Query.KILOMETERS);
                         }
-                        System.out.println("@" + BLUE + status.getUser().getScreenName() + RESET +
-                                ": " + status.getText() + retweetCount(status));
+                    }
 
-                        if (isStreamEnabled) {
-                            try {
-                                Thread.sleep(1000); // sleep for 1 second
-                            } catch (InterruptedException e) {
-                                Thread.currentThread().interrupt();
+                    QueryResult result = twitter.search(query); // send a query
+
+                    for (Status status : result.getTweets()) { // print tweets
+                        if ((!status.isRetweet() || !hideRetweets)) { // if necessary, hide retweets
+
+                            if (!isStreamEnabled) {
+                                System.out.print("[" + getDate(status.getCreatedAt()) + "] ");
                             }
+
+                            System.out.println("@" + BLUE + status.getUser().getScreenName() + RESET +
+                                    (status.isRetweet() ? " ретвитнул " + "@" + BLUE +
+                                            status.getRetweetedStatus().getUser().getScreenName() + RESET : "") +
+                                    ": " + status.getText() + retweetCount(status));
+
+                            if (isStreamEnabled) {
+                                try {
+                                    Thread.sleep(1000); // sleep for 1 second
+                                } catch (InterruptedException e) {
+                                    Thread.currentThread().interrupt();
+                                }
+                            }
+
                         }
                     }
-                    i++;
-                }
 
-                if (result.getTweets().isEmpty()) { // nothing was found
-                    System.out.println("По вашему запросу ничего не найдено.");
-                }
-                break; // no need to try again
-            } catch (TwitterException e) {
-                if (e.isCausedByNetworkIssue()) {
-                    System.err.println("Ошибка соединения: " + e.getErrorMessage() + ". Повторная попытка...");
-
-                    try {
-                        Thread.sleep(1000); // sleep for 1 second
-                    } catch (InterruptedException ie) {
-                        Thread.currentThread().interrupt();
+                    if (result.getTweets().isEmpty()) { // nothing was found
+                        System.out.println("По вашему запросу ничего не найдено.");
                     }
+                    break; // no need to try again
+                } catch (TwitterException e) {
+                    if (e.isCausedByNetworkIssue()) {
+                        System.err.println("Ошибка соединения: " + e.getErrorMessage() + ". Повторная попытка...");
 
-                    // try again
-                    if (++attempts == MAX_ATTEMPTS) {
-                        System.err.println("Не удалось.");
+                        try {
+                            Thread.sleep(1000); // sleep for 1 second
+                        } catch (InterruptedException ie) {
+                            Thread.currentThread().interrupt();
+                        }
+
+                        // try again
+                        if (++attempts == MAX_ATTEMPTS) {
+                            System.err.println("Не удалось.");
+                            break;
+                        }
+
+                    } else {
+                        System.err.println("Ошибка TwitterAPI: " + e.getErrorMessage());
                         break;
                     }
-
-                } else {
-                    System.err.println("Ошибка TwitterAPI: " + e.getErrorMessage());
-                    break;
                 }
             }
+        } catch (UnknownHostException e) {
+            System.err.println("Unknown host");
         }
 
     }
@@ -224,10 +271,6 @@ public class App {
         return status.getRetweetCount() > 0 ? "" + status.getRetweetCount() : "";
     }
 
-    protected static String getLocation(Status status) {
-        return (status.getGeoLocation() != null) ? "" + status.getGeoLocation().toString() : "";
-    }
-
     protected static void showHelp() {
         File file = new File(HELP);
 
@@ -254,20 +297,6 @@ public class App {
             System.err.println("Нет файла help");
         } catch (IOException e) {
             System.err.println("Проблема с чтением файла");
-        }
-    }
-
-    public static void clearScreen() {
-        try {
-            final String os = System.getProperty("os.name");
-
-            if (os.contains("Windows")) {
-                Runtime.getRuntime().exec("cls");
-            } else {
-                Runtime.getRuntime().exec("clear");
-            }
-        } catch (IOException e) {
-            System.err.println("Невозможно очистить консоль");
         }
     }
 
