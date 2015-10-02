26c6f35ecc8a1837d8809f19ed32a628ac341aa7
tulindanil <tulindanil@gmail.com>
2015-10-02 23:18:19 +0300

    fixed braces

--- solutions/tulindanil/0_Reverser/Reverser.java
+++ solutions/tulindanil/0_Reverser/Reverser.java
@@ -1,12 +1,8 @@
-public class Reverser
-{
-    public static void main(String[] args)
-    {
-        for (int index = args.length - 1; index >= 0; --index)
-        {
+public class Reverser {
+    public static void main(String[] args) {
+        for (int index = args.length - 1; index >= 0; --index) {
             String[] numbers = args[index].split("\\s");
-            for (int numberIdx = numbers.length - 1; numberIdx >= 0; --numberIdx)
-            {
+            for (int numberIdx = numbers.length - 1; numberIdx >= 0; --numberIdx) {
                 System.out.print(numbers[numberIdx] + " ");
             }
         }
