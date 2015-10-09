42751cab89057266a692cd2d29485e1bfd63c223
Anna Vlasova <anna.vlasova@phystech.edu>
2015-10-09 15:26:39 +0300

    Bad directory deleted

--- tasks/task00/Reverser.java
+++
@@ -1,9 +0,0 @@
-public class Reverser {
-public static void main(String[] args) {
-for (int i = args.length - 1; i >= 0; i--) {
-String[] a = args[i].split("\\s+");
-for (int j = a.length - 1; j >= 0; j--)
-System.out.print(a[j] + " ");
-}
-}
-}
