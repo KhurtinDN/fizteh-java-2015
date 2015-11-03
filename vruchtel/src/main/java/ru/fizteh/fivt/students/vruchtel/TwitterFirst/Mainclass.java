3654409aa587d48fbd59ca55a8d6e707aecf8a76
Vruchtel <vruchtel.serafima@yandex.ru>
2015-11-04 00:55:38 +0300

    Added the second version of TwitterStream
    Now it is ready and working

--- vruchtel/src/main/java/ru/fizteh/fivt/students/vruchtel/TwitterFirst/Mainclass.java
+++ vruchtel/src/main/java/ru/fizteh/fivt/students/vruchtel/TwitterFirst/Mainclass.java
@@ -4,74 +4,119 @@ package ru.fizteh.fivt.students.vruchtel.TwitterFirst;
  */
 import twitter4j.Status;
 //import twitter4j.Twitter;
-//import twitter4j.TwitterFactory;
+import twitter4j.TwitterFactory;
 import twitter4j.auth.AccessToken;
 import jdk.net.SocketFlow;
 import twitter4j.*;
 import com.beust.jcommander.*;
 
+import java.io.IOException;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
+
 public class Mainclass {
 
     public static void main(String args[]) throws Exception {
-        twitterStreamArgs = new TwitterStreamArgs(args);
+        try {
+            twitterStreamArgs = new TwitterStreamArgs(args);
+            textFormatter = new TextFormatter();
+            System.out.println("Твиты по запросу <" + twitterStreamArgs.getKeyword() + ">:");
 
+            if (twitterStreamArgs.isStreamUse()) {
+                workInStreamMode(); //если задан флаг --stream
+            } else {
+                workInCommonMode(); //если флаг --stream не задан
+            }
+
+            System.exit(0);
+
+        } catch (TwitterException exception) {
+            System.err.println("Twitter exception!");
+        } catch (IOException exception) {
+            System.err.println("Problems with printing or reading data!");
+        } catch (InterruptedException exception) {
+            System.err.println("Problems with sleeping!");
+        }
+
+    }
+
+    private static StatusListener tweetListener = new StatusAdapter(){
+        public void onStatus(Status status) {
+            if (!twitterStreamArgs.isHideRetweets() || !status.isRetweet()) {
+                streamQueue.add(status);
+            }
+        }
+    };
+
+    //если задан флаг --stream
+    public static void workInStreamMode() throws Exception {
+        streamQueue = new ArrayBlockingQueue<Status>(QUEUE_MAX_SIZE);
+        TwitterStream twitterStream = new TwitterStreamFactory().getInstance();
+        twitterStream.addListener(tweetListener);
+        FilterQuery filterQuery = new FilterQuery();
+        filterQuery.track(new String[]{twitterStreamArgs.getKeyword()});
+        twitterStream.filter(filterQuery);
+
+        Boolean haveToFinish = false;
+        int readedSymbol;
+        //выполняться, пока не прервёмся
+        while (true) {
+            while(!streamQueue.isEmpty()) {
+                Status status = streamQueue.poll();
+                //System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+                System.out.println(textFormatter.getTextToPrint(status, false));
+            }
+
+            //проверим, надо ли завершиться, и, если надо, завершимся
+            while (System.in.available() > 0) {
+                readedSymbol = System.in.read();
+                if(readedSymbol == 'q' || readedSymbol == EXIT_KEY || readedSymbol == -1) {
+                    twitterStream.shutdown();
+                    haveToFinish = true;
+                    break;
+                }
+            }
+            if(haveToFinish) break;
+            Thread.sleep(SLEEP_TIME);
+        }
+    }
+
+    //если флаг --stream не задан
+    public static void workInCommonMode() throws Exception {
         twitter = new TwitterFactory().getSingleton();
         //TwitterFactory().getInstance() возвращает просто Twitter, а TwitterFactory().GetSingleton()
         //возвращает static Twitter
 
-        makeQuery();
-
-        if(twitterStreamArgs.isStreamUse()) {
-            workInStreamMode(); //если задан флаг --stream
-        } else {
-            workInCommonMode(); //если флаг --stream не задан
-        }
-
-        System.exit(0);
-
-
-        //Status status = twitter.updateStatus("Omnomnomnom");
-        //System.out.println("Successfully updated the status to [" + status.getText() + "].");
-        //System.exit(0);
-
-    }
-
-    //нужно сделать запрос, по которому будем искать
-    public static void makeQuery() {
-        String addToTheQuery = "";
-        if(twitterStreamArgs.isHideRetweets()) {
-            addToTheQuery = "+exclude:retweets";
-        }
-        //отладочный вывод (потом не забыть убрать)
-        System.out.println("The query is: " + twitterStreamArgs.getKeyword() + addToTheQuery);
-
-        //инициализируем запрос
-        String queryKeywords = twitterStreamArgs.getKeyword() + addToTheQuery;
-        System.out.println("The queryKeywords is: " + queryKeywords);
-        query = new Query(queryKeywords);
         query = new Query(twitterStreamArgs.getKeyword());
-
-        //ещё у query есть метод setCount - это чтобы задавать количество
-
-
-        //тут надо будет разобраться с place, пока забьём на это
-        //можно использовать что-то типа SetGeo
-    }
-
-    //если задан флаг --stream
-    public static void workInStreamMode() {}
-
-    //если флаг --stream не задан
-    public static void workInCommonMode() throws Exception {
         queryResult = twitter.search(query);
-        for(twitter4j.Status status : queryResult.getTweets()) {
-           //для красивого вывода вообще нужно будет написать отдельный класс
-           System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-       }
+        Integer tweetsCounter = 0;
+        while (tweetsCounter++ < twitterStreamArgs.getLimit()) {
+            for (twitter4j.Status status : queryResult.getTweets()) {
+                //для красивого вывода вообще нужно будет написать отдельный класс
+                if (!twitterStreamArgs.isHideRetweets() || !status.isRetweet()) {
+                    //System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
+                    System.out.println(textFormatter.getTextToPrint(status, true));
+                }
+
+                if (tweetsCounter++ >= twitterStreamArgs.getLimit()) break;
+
+            }
+            if (queryResult.hasNext()) {//есть ли следующая страничка
+                queryResult = twitter.search(queryResult.nextQuery());
+            } else {
+                break;
+            }
+        }
     }
 
     private static Twitter twitter;
     private static TwitterStreamArgs twitterStreamArgs;
     private static Query query;
     private static QueryResult queryResult;
+    private static BlockingQueue<Status> streamQueue;
+    private static TextFormatter textFormatter;
+    private static final int QUEUE_MAX_SIZE = 1000;
+    private static final int SLEEP_TIME = 1000;
+    private static final char EXIT_KEY = (char) 27;
 }
--- vruchtel/src/main/java/ru/fizteh/fivt/students/vruchtel/TwitterFirst/TextFormatter.java
+++ vruchtel/src/main/java/ru/fizteh/fivt/students/vruchtel/TwitterFirst/TextFormatter.java
@@ -6,11 +6,86 @@ package ru.fizteh.fivt.students.vruchtel.TwitterFirst;
 
 import twitter4j.Status;
 
-public class TextFormatter {
-    //todo в этом классе надо будет описать, как форматируется текст, который необходимо вывести
-    static String getTextToPrint(Status tweet) {
-        String str = new String();
+import java.util.Stack;
+import java.util.GregorianCalendar;
+import java.util.Date;
+import java.util.Calendar;
 
-        return str;
+public class TextFormatter {
+    static boolean isToday(GregorianCalendar date) {
+        GregorianCalendar todayDate = new GregorianCalendar();
+
+        return  todayDate.get(Calendar.DAY_OF_MONTH) == date.get(Calendar.DAY_OF_MONTH)
+                && todayDate.get(Calendar.MONTH) == date.get(Calendar.MONTH)
+                && todayDate.get(Calendar.YEAR) == date.get(Calendar.YEAR);
     }
+
+    static boolean isYesterday(GregorianCalendar date) {
+        GregorianCalendar yesterdayDate = new GregorianCalendar();
+        yesterdayDate.add(Calendar.DAY_OF_MONTH, -1);
+
+        return  yesterdayDate.get(Calendar.DAY_OF_MONTH) == date.get(Calendar.DAY_OF_MONTH)
+                && yesterdayDate.get(Calendar.MONTH) == date.get(Calendar.MONTH)
+                && yesterdayDate.get(Calendar.YEAR) == date.get(Calendar.YEAR);
+    }
+
+    static String getTweetTime(Status status) {
+        String printingTime = "";
+        GregorianCalendar tweetTime = new GregorianCalendar();
+        tweetTime.setTime(new Date(status.getCreatedAt().getTime()));
+
+        Long passedTime = (new Date().getTime() - status.getCreatedAt().getTime());
+
+        if(passedTime < TWO_MINUTES) {
+            printingTime += "Только что";
+        } else if(passedTime < HOUR) {
+            Long minutes = passedTime / MINUTE;
+            printingTime += minutes.toString() + " минут назад";
+        } else if(isToday(tweetTime)) { //не работает, кажись
+            Long hours = passedTime / HOUR;
+            printingTime += hours.toString() + " часов назад";
+        } else if(isYesterday(tweetTime)) {
+            printingTime += "Вчера";
+        } else {
+            Long days = passedTime / DAY;
+            printingTime += days.toString() + " дней назад";
+        }
+
+        return printingTime;
+    }
+
+    static String getColorisedNick(Status status) {
+        return ANSI_BLUE + "@" + status.getUser().getScreenName() + ": " + ANSI_RESET;
+    }
+
+    static String getTextToPrint(Status status, boolean printTime) {
+        String printingText = "";
+
+        if(printTime) {
+            printingText += "[" + getTweetTime(status) + "] ";
+        }
+
+        printingText += getColorisedNick(status);
+
+        if(status.isRetweet()) {
+            printingText += "ретвитнул " + getColorisedNick(status.getRetweetedStatus());
+        }
+
+        printingText += status.getText();
+
+        if(!status.isRetweet() && status.isRetweeted()) {
+            printingText += " (" + status.getRetweetCount() + " ретвитов)";
+        }
+
+        return printingText;
+    }
+
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+    public static final Long SECOND = 1000L;
+    public static final Long MINUTE = SECOND * 60;
+    public static final Long TWO_MINUTES = MINUTE * 2;
+    public static final Long HOUR = MINUTE * 60;
+    public static final Long DAY = HOUR * 24;
 }
