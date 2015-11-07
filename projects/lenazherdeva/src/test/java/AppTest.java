6e5568dc115a33a89eef050ba1981214c9250bed
lenazherdeva <lena-zherdeva@mail.ru>
2015-11-07 15:10:57 +0300

    Changed

--- projects/lenazherdeva/src/test/java/AppTest.java
+++ projects/lenazherdeva/src/test/java/AppTest.java
@@ -1,6 +1,6 @@
 /**
  * Created by admin on 05.11.2015.
- */
+ *//*
 public class AppTest {
 
-}
+}*/
--- projects/lenazherdeva/src/test/java/TimeTest.java
+++ projects/lenazherdeva/src/test/java/TimeTest.java
@@ -8,11 +8,11 @@ import org.mockito.runners.MockitoJUnitRunner;
  */
 
 
-
+/*
 @RunWith(MockitoJUnitRunner.class)
 public class TimeTest extends TestCase {
 
     @Test
     public
 
-}
+}*/
