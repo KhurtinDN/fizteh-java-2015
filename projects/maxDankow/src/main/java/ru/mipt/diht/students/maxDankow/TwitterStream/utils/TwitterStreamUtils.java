e9f279c1d8349b8c745f10e9555b2941c21826c2
Maxim Dankovtsev <max.dankow@gmail.com>
2015-11-08 23:00:18 +0300

    Codestyle fixes.

--- projects/maxDankow/src/main/java/ru/mipt/diht/students/maxDankow/TwitterStream/utils/TwitterStreamUtils.java
+++ projects/maxDankow/src/main/java/ru/mipt/diht/students/maxDankow/TwitterStream/utils/TwitterStreamUtils.java
@@ -16,15 +16,15 @@ public class TwitterStreamUtils {
     private static final int DELIM_LENGTH = 200;
 
     public enum TextColor {
-        CLEAR (0),
-        BLACK (30),
-        RED (31),
-        GREEN (32),
-        YELLOW (33),
-        BLUE (34),
-        MAGENTA (35),
-        CYAN (36),
-        WHITE (37);
+        CLEAR(0),
+        BLACK(30),
+        RED(31),
+        GREEN(32),
+        YELLOW(33),
+        BLUE(34),
+        MAGENTA(35),
+        CYAN(36),
+        WHITE(37);
 
         private int colorCode;
 
@@ -47,18 +47,16 @@ public class TwitterStreamUtils {
         if (ChronoUnit.MINUTES.between(anotherTime, currentTime) < 2) {
             return "Только что";
         }
-        if (ChronoUnit.HOURS.between(anotherTime, currentTime) < 1)
-        {
+        if (ChronoUnit.HOURS.between(anotherTime, currentTime) < 1) {
             return "" + ChronoUnit.MINUTES.between(anotherTime, currentTime) + " минут назад";
         }
-        if (ChronoUnit.DAYS.between(anotherTime, currentTime) < 1)
-        {
+        if (ChronoUnit.DAYS.between(anotherTime, currentTime) < 1) {
             return "" + ChronoUnit.HOURS.between(anotherTime, currentTime) + " часов назад";
         }
         if (ChronoUnit.DAYS.between(anotherTime, currentTime) == 1) {
             return "Вчера";
         }
-        return "" + ChronoUnit.DAYS.between(anotherTime, currentTime) +  " дней назад";
+        return "" + ChronoUnit.DAYS.between(anotherTime, currentTime) + " дней назад";
     }
 
     public static boolean checkTweet(Status tweet, Geometry locationGeometry,
