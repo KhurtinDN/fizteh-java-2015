cb81bfc50d86c6fbd094039a1b3f683daa3cb26b
Denis Khurtin <dkhurtin@yandex-team.ru>
2015-11-14 13:15:40 +0300

    Add task4

---
+++ projects/dkhurtin/src/main/java/ru/mipt/diht/students/dkhurtin/collectionquery2/Sources.java
@@ -0,0 +1,53 @@
+package ru.mipt.diht.students.dkhurtin.collectionquery2;
+
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Stream;
+
+/**
+ * Helper methods to create collections.
+ */
+public class Sources {
+
+    /**
+     * @param items
+     * @param <T>
+     * @return
+     */
+    @SafeVarargs
+    public static <T> List<T> list(T... items) {
+        return Arrays.asList(items);
+    }
+
+    /**
+     * @param items
+     * @param <T>
+     * @return
+     */
+    @SafeVarargs
+    public static <T> Set<T> set(T... items) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param inputStream
+     * @param <T>
+     * @return
+     */
+    public static <T> Stream<T> lines(InputStream inputStream) {
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * @param file
+     * @param <T>
+     * @return
+     */
+    public static <T> Stream<T> lines(Path file) {
+        throw new UnsupportedOperationException();
+    }
+
+}
