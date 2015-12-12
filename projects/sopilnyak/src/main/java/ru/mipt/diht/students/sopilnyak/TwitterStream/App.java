0124a2467a466d23ec0fdf9b292f63ba4e489700
sopilnyak <o.sopilniak@gmail.com>
2015-12-12 15:45:40 +0300

    Update TwitterStream

--- projects/sopilnyak/src/main/java/ru/mipt/diht/students/sopilnyak/TwitterStream/App.java
+++ projects/sopilnyak/src/main/java/ru/mipt/diht/students/sopilnyak/TwitterStream/App.java
@@ -11,31 +11,31 @@ import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    private static String queryString;
-    private static boolean isQueryStarted = false;
-    private static String locationString;
-    private static boolean isLocationStarted = false;
-    private static boolean isNearbyEnabled = false;
-    private static boolean isStreamEnabled = false;
-    private static boolean isSetLimitStarted = false;
-    private static int limit = -1;
-    private static boolean hideRetweets = false;
+    protected static String queryString;
+    protected static boolean isQueryStarted = false;
+    protected static String locationString;
+    protected static boolean isLocationStarted = false;
+    protected static boolean isNearbyEnabled = false;
+    protected static boolean isStreamEnabled = false;
+    protected static boolean isSetLimitStarted = false;
+    protected static int limit = -1;
+    protected static boolean hideRetweets = false;
 
     public static final String BLUE = "\u001B[34m";
     public static final String RESET = "\u001B[0m";
-    public static final String HELP = "help.txt";
+    public static final String HELP_FILE = "help.txt";
     public static final int MAX_TWEETS = 100;
     public static final int RADIUS = 10;
     public static final int SLEEP_TIME = 1000;
     public static final int ATTEMPTS = 20;
 
-    public static final int C1 = 1;
-    public static final int C2 = 2;
-    public static final int C3 = 3;
-    public static final int C4 = 4;
-    public static final int C5 = 5;
-    public static final int C6 = 6;
-    public static final int C7 = 7;
+    public static final int START_QUERY = 1;
+    public static final int LOCATION = 2;
+    public static final int STREAM_MODE = 3;
+    public static final int HIDE_RETWEETS = 4;
+    public static final int SET_LIMIT = 5;
+    public static final int HELP = 6;
+    public static final int NEARBY = 7;
 
     public static void main(String[] args) {
 
@@ -56,43 +56,43 @@ public class App {
                     }
                     break;
 
-                case C1: // start new query
+                case START_QUERY: // start new query
                     queryString = "";
                     isQueryStarted = true;
                     isLocationStarted = false;
                     break;
 
-                case C2: // location
+                case LOCATION: // location
                     locationString = "";
                     isLocationStarted = true;
                     isQueryStarted = false;
                     break;
 
-                case C3: // stream mode
+                case STREAM_MODE: // stream mode
                     isStreamEnabled = true;
                     isQueryStarted = false;
                     isLocationStarted = false;
                     break;
 
-                case C4: // hide retweets
+                case HIDE_RETWEETS: // hide retweets
                     hideRetweets = true;
                     isQueryStarted = false;
                     isLocationStarted = false;
                     break;
 
-                case C5: // set limit
+                case SET_LIMIT: // set limit
                     isSetLimitStarted = true;
                     isQueryStarted = false;
                     isLocationStarted = false;
                     break;
 
-                case C6: // help
+                case HELP: // help
                     isQueryStarted = false;
                     isLocationStarted = false;
                     showHelp();
                     break;
 
-                case C7: // nearby
+                case NEARBY: // nearby
                     isNearbyEnabled = true;
                     isQueryStarted = false;
                     isLocationStarted = false;
@@ -119,232 +119,38 @@ public class App {
                 || queryString.equals("")) && locationString == null) {
             System.err.println("No query, nothing to find");
         } else {
-            addQuery();
+            TwitterAPI.addQuery();
         }
 
     }
 
     protected static short commandNumber(String arg) {
         if (arg.equals("--query") || arg.equals("-q")) {
-            return C1;
+            return START_QUERY;
         }
         if (arg.equals("--place") || arg.equals("-p")) {
-            return C2;
+            return LOCATION;
         }
         if (arg.equals("--stream") || arg.equals("-s")) {
-            return C3;
+            return STREAM_MODE;
         }
         if (arg.equals("--hideRetweets")) {
-            return C4;
+            return HIDE_RETWEETS;
         }
         if (arg.equals("--limit") || arg.equals("-l")) {
-            return C5;
+            return SET_LIMIT;
         }
         if (arg.equals("--help") || arg.equals("-h")) {
-            return C6;
+            return HELP;
         }
         if (arg.equals("nearby") && isLocationStarted) {
-            return C7;
+            return NEARBY;
         }
         return 0;
     }
 
-    protected static void addQuery() {
-
-        Twitter twitter = TwitterFactory.getSingleton();
-        Query query = new Query(queryString);
-        GeoQuery geoQuery;
-        try {
-            String ip = Inet4Address.getLocalHost().getHostAddress();
-            geoQuery = new GeoQuery(ip);
-
-            query.setCount(MAX_TWEETS); // max number of tweets
-            if (limit != -1) {
-                query.setCount(limit);
-            }
-
-            System.out.print("Твиты");
-            if (queryString != null && !queryString.equals("")) {
-                System.out.print(" по запросу \"" + queryString + "\"");
-            }
-            if (isStreamEnabled) {
-                System.out.print(" в режиме потока");
-            }
-            if (locationString != null && !locationString.equals("")
-                    && !isNearbyEnabled) {
-                System.out.print(" возле местоположения \""
-                        + locationString + "\"");
-                geoQuery.setQuery(locationString);
-            }
-            if (locationString != null && (isNearbyEnabled
-                    || locationString.equals(""))) {
-                System.out.print(" возле вашего местоположения");
-                isNearbyEnabled = true;
-            } else {
-                isNearbyEnabled = false;
-            }
-            System.out.println(":");
-
-            int attempts = 0;
-
-            // print the results of the query
-            while (true) {
-
-                try {
-
-                    if (locationString != null) {
-                        ResponseList<Place> places =
-                                twitter.searchPlaces(geoQuery);
-
-                        if (places.size() == 0) {
-                            System.out.
-                                    println("Невозможно определить "
-                                            + "местоположение");
-                            return;
-
-                        } else {
-                            Place place = places.get(0); // get first place
-                            // search in radius by coordinates
-                            query.setGeoCode(place.
-                                            getBoundingBoxCoordinates()[0][0],
-                                    RADIUS, Query.KILOMETERS);
-                        }
-                    }
-
-                    if (!isStreamEnabled) {
-
-                        QueryResult result = twitter.search(query);
-                        // send a query
-
-                        for (Status status : result.getTweets()) {
-                            // print tweets
-                            if ((!status.isRetweet()
-                                    || !hideRetweets)) { // hide retweets
-
-                                System.out.print("["
-                                        + getDate(status.getCreatedAt())
-                                        + "] ");
-
-                                System.out.println("@" + BLUE
-                                        + status.getUser().getScreenName()
-                                        + RESET + getRetweetSource(status)
-                                        + ": " + status.getText()
-                                        + retweetCount(status));
-                            }
-                        }
-
-                        if (result.getTweets().isEmpty()) {
-                            // nothing was found
-                            System.out.println("По вашему запросу "
-                                    + "ничего не найдено.");
-                        }
-
-                    } else {
-
-                        while (true) {
-                            QueryResult result = twitter.search(query);
-                            for (Status status : result.getTweets()) {
-                                if (!status.isRetweet() || !hideRetweets) {
-                                    // hide retweets
-
-                                    System.out.println("@" + BLUE
-                                            + status.getUser().getScreenName()
-                                            + RESET + getRetweetSource(status)
-                                            + ": " + status.getText()
-                                            + retweetCount(status));
-
-                                    try {
-                                        Thread.sleep(SLEEP_TIME);
-                                        // sleep for 1 second
-                                    } catch (InterruptedException e) {
-                                        Thread.currentThread().interrupt();
-                                    }
-                                }
-                            }
-
-                            if (!result.getTweets().isEmpty()) {
-                                Status status = result.getTweets().get(0);
-                                query.setSinceId(status.getId());
-                            }
-                        }
-                    }
-                    break; // no need to try again
-
-                } catch (TwitterException e) {
-                    if (e.isCausedByNetworkIssue()) {
-                        System.err.println("Ошибка соединения: "
-                                + e.getErrorMessage()
-                                + ". Повторная попытка...");
-                        try {
-                            Thread.sleep(SLEEP_TIME); // sleep for 1 second
-                        } catch (InterruptedException ie) {
-                            Thread.currentThread().interrupt();
-                        }
-                        // try again
-                        if (++attempts == ATTEMPTS) {
-                            System.err.println("Не удалось.");
-                            break;
-                        }
-                    } else {
-                        System.err.println("Ошибка TwitterAPI: "
-                                + e.getErrorMessage());
-                        break;
-                    }
-                }
-            }
-        } catch (UnknownHostException e) {
-            System.err.println("Unknown host");
-        }
-
-    }
-
-    protected static String getDate(Date date) {
-
-        Calendar currentCal = Calendar.getInstance();
-        Calendar calendar = Calendar.getInstance();
-        calendar.setTime(date);
-        Date current = currentCal.getTime();
-
-        final long diffFull = current.getTime() - date.getTime();
-
-        if (calendar.get(Calendar.YEAR)
-                == currentCal.get(Calendar.YEAR)
-                && calendar.get(Calendar.DAY_OF_YEAR)
-                == currentCal.get(Calendar.DAY_OF_YEAR) - 1) { // yesterday
-            return "Вчера";
-        }
-
-        long diffDays = TimeUnit.DAYS.convert(
-                diffFull, TimeUnit.MILLISECONDS);
-        if (diffDays > 0) {
-            return "" + diffDays + " дней назад";
-        }
-
-        long diffHours = TimeUnit.HOURS.convert(
-                diffFull, TimeUnit.MILLISECONDS);
-        if (diffHours >= 1) {
-            return "" + diffHours + " часов назад";
-        }
-
-        long diffMinutes = TimeUnit.MINUTES.convert(
-                diffFull, TimeUnit.MILLISECONDS);
-        if (diffMinutes >= 2) {
-            return "" + diffMinutes + " минут назад";
-        }
-
-        return "Только что";
-
-    }
-
-    protected static String retweetCount(Status status) {
-        if (status.getRetweetCount() > 0) {
-            return status.getRetweetCount() + "";
-        }
-        return "";
-    }
-
     protected static void showHelp() {
-        File file = new File(HELP);
+        File file = new File(HELP_FILE);
 
         try {
 
@@ -371,16 +177,6 @@ public class App {
         } catch (IOException e) {
             System.err.println("Проблема с чтением файла");
         }
-    }
-
-    protected static String getRetweetSource(Status status) {
-        if (status.isRetweet()) {
-            return " ретвитнул "
-                    + "@" + BLUE
-                    + status.getRetweetedStatus().
-                    getUser().getScreenName() + RESET;
-        }
-        return "";
     }
 
 }
--- projects/sopilnyak/src/main/java/ru/mipt/diht/students/sopilnyak/TwitterStream/TwitterAPI.java
+++ projects/sopilnyak/src/main/java/ru/mipt/diht/students/sopilnyak/TwitterStream/TwitterAPI.java
@@ -20,7 +20,6 @@ public class TwitterAPI extends App {
 
     protected static void addQuery() {
 
-        Twitter twitter = TwitterFactory.getSingleton();
         Query query = new Query(queryString);
         GeoQuery geoQuery;
         try {
@@ -54,117 +53,133 @@ public class TwitterAPI extends App {
             }
             System.out.println(":");
 
-            int attempts = 0;
+            printResults(query, geoQuery);
 
-            // print the results of the query
-            while (true) {
-
-                try {
-
-                    if (locationString != null) {
-                        ResponseList<Place> places =
-                                twitter.searchPlaces(geoQuery);
-
-                        if (places.size() == 0) {
-                            System.out.
-                                    println("Невозможно определить "
-                                            + "местоположение");
-                            return;
-
-                        } else {
-                            Place place = places.get(0); // get first place
-                            // search in radius by coordinates
-                            query.setGeoCode(place.
-                                            getBoundingBoxCoordinates()[0][0],
-                                    RADIUS, Query.KILOMETERS);
-                        }
-                    }
-
-                    if (!isStreamEnabled) {
-
-                        QueryResult result = twitter.search(query);
-                        // send a query
-
-                        for (Status status : result.getTweets()) {
-                            // print tweets
-                            if ((!status.isRetweet()
-                                    || !hideRetweets)) { // hide retweets
-
-                                System.out.print("["
-                                        + getDate(status.getCreatedAt())
-                                        + "] ");
-
-                                System.out.println("@" + BLUE
-                                        + status.getUser().getScreenName()
-                                        + RESET + getRetweetSource(status)
-                                        + ": " + status.getText()
-                                        + retweetCount(status));
-                            }
-                        }
-
-                        if (result.getTweets().isEmpty()) {
-                            // nothing was found
-                            System.out.println("По вашему запросу "
-                                    + "ничего не найдено.");
-                        }
-
-                    } else {
-
-                        while (true) {
-                            QueryResult result = twitter.search(query);
-                            for (Status status : result.getTweets()) {
-                                if (!status.isRetweet() || !hideRetweets) {
-                                    // hide retweets
-
-                                    System.out.println("@" + BLUE
-                                            + status.getUser().getScreenName()
-                                            + RESET + getRetweetSource(status)
-                                            + ": " + status.getText()
-                                            + retweetCount(status));
-
-                                    try {
-                                        Thread.sleep(SLEEP_TIME);
-                                        // sleep for 1 second
-                                    } catch (InterruptedException e) {
-                                        Thread.currentThread().interrupt();
-                                    }
-                                }
-                            }
-
-                            if (!result.getTweets().isEmpty()) {
-                                Status status = result.getTweets().get(0);
-                                query.setSinceId(status.getId());
-                            }
-                        }
-                    }
-                    break; // no need to try again
-
-                } catch (TwitterException e) {
-                    if (e.isCausedByNetworkIssue()) {
-                        System.err.println("Ошибка соединения: "
-                                + e.getErrorMessage()
-                                + ". Повторная попытка...");
-                        try {
-                            Thread.sleep(SLEEP_TIME); // sleep for 1 second
-                        } catch (InterruptedException ie) {
-                            Thread.currentThread().interrupt();
-                        }
-                        // try again
-                        if (++attempts == ATTEMPTS) {
-                            System.err.println("Не удалось.");
-                            break;
-                        }
-                    } else {
-                        System.err.println("Ошибка TwitterAPI: "
-                                + e.getErrorMessage());
-                        break;
-                    }
-                }
-            }
         } catch (UnknownHostException e) {
             System.err.println("Unknown host");
         }
 
+    }
+
+    protected static void printResults(Query query, GeoQuery geoQuery) {
+        Twitter twitter = TwitterFactory.getSingleton();
+        int attempts = 0;
+
+        // print the results of the query
+        while (true) {
+
+            try {
+
+                if (locationString != null) {
+                    ResponseList<Place> places =
+                            twitter.searchPlaces(geoQuery);
+
+                    if (places.size() == 0) {
+                        System.out.
+                                println("Невозможно определить "
+                                        + "местоположение");
+                        return;
+
+                    } else {
+                        Place place = places.get(0); // get first place
+                        // search in radius by coordinates
+                        query.setGeoCode(place.
+                                        getBoundingBoxCoordinates()[0][0],
+                                RADIUS, Query.KILOMETERS);
+                    }
+                }
+
+                if (!isStreamEnabled) {
+
+                    QueryResult result = twitter.search(query);
+                    getResultsStreamNotEnabled(result);
+
+                    if (result.getTweets().isEmpty()) {
+                        // nothing was found
+                        System.out.println("По вашему запросу "
+                                + "ничего не найдено.");
+                    }
+
+                } else {
+
+                    QueryResult result = twitter.search(query);
+                    getResultsStreamEnabled(result, query);
+                }
+
+                break; // no need to try again
+
+            } catch (TwitterException e) {
+                if (e.isCausedByNetworkIssue()) {
+                    System.err.println("Ошибка соединения: "
+                            + e.getErrorMessage()
+                            + ". Повторная попытка...");
+                    try {
+                        Thread.sleep(SLEEP_TIME); // sleep for 1 second
+                    } catch (InterruptedException ie) {
+                        Thread.currentThread().interrupt();
+                    }
+                    // try again
+                    if (++attempts == ATTEMPTS) {
+                        System.err.println("Не удалось.");
+                        break;
+                    }
+                } else {
+                    System.err.println("Ошибка TwitterAPI: "
+                            + e.getErrorMessage());
+                    break;
+                }
+            }
+        }
+    }
+
+    protected static void getResultsStreamNotEnabled(QueryResult result) throws TwitterException {
+
+        // send a query
+
+        for (Status status : result.getTweets()) {
+            // print tweets
+            if ((!status.isRetweet()
+                    || !hideRetweets)) { // hide retweets
+
+                System.out.print("["
+                        + getDate(status.getCreatedAt())
+                        + "] ");
+
+                System.out.println("@" + BLUE
+                        + status.getUser().getScreenName()
+                        + RESET + getRetweetSource(status)
+                        + ": " + status.getText()
+                        + retweetCount(status));
+            }
+        }
+    }
+
+    protected static void getResultsStreamEnabled(QueryResult result, Query query) throws TwitterException {
+        while (true) {
+            for (Status status : result.getTweets()) {
+                if (!status.isRetweet() || !hideRetweets) {
+                    // hide retweets
+
+                    System.out.println("@" + BLUE
+                            + status.getUser().getScreenName()
+                            + RESET + getRetweetSource(status)
+                            + ": " + status.getText()
+                            + retweetCount(status));
+
+                    try {
+                        Thread.sleep(SLEEP_TIME);
+                        // sleep for 1 second
+                    } catch (InterruptedException e) {
+                        Thread.currentThread().interrupt();
+                    }
+                }
+            }
+
+            if (!result.getTweets().isEmpty()) {
+                Status status = result.getTweets().get(0);
+                query.setSinceId(status.getId());
+            }
+        }
     }
 
     protected static String getDate(Date date) {
