5168b2a458ec55c13b2264bb9e36fb4ab3910383
lenazherdeva <lena-zherdeva@mail.ru>
2015-12-13 18:31:56 +0300

    threads started

---
+++ projects/lenazherdeva/src/main/java/ru/mipt/diht/students/lenazherdeva/threads/Counter.java
@@ -0,0 +1,36 @@
+package ru.mipt.diht.students.lenazherdeva.threads;
+
+/**
+ * Created by admin on 13.12.2015.
+ */
+public class Counter extends Thread {
+
+    private static int numberOfThreads;
+    private int numberOfThisThread;
+    private static Object object = new Object();
+    private static volatile int currentThread = 1;
+
+    Counter(int numberOfTread, int number) {
+        numberOfThisThread = numberOfTread;
+        numberOfThreads = number;
+    }
+
+    @Override
+    public final void run() {
+        synchronized (object) {
+            while (true) {
+                if (currentThread == numberOfThisThread) {
+                    System.out.println("Thread-" + currentThread);
+                    currentThread %= numberOfThreads;
+                    currentThread++;
+                    object.notifyAll();
+                }
+                try {
+                    object.wait();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+}
