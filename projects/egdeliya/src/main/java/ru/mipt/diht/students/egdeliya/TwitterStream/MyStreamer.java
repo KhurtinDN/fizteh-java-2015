fde7a357ffee64a92c5923d5518846da6e63e6a6
egdeliya <egdeliya@mail.ru>
2015-12-13 15:57:08 +0300

    third commit

---
+++ projects/egdeliya/src/main/java/ru/mipt/diht/students/egdeliya/TwitterStream/MyStreamer.java
@@ -0,0 +1,10 @@
+package ru.mipt.diht.students.egdeliya.TwitterStream;
+
+/**
+ * Created by Эгделия on 13.12.2015.
+ */
+public class MyStreamer {
+    public static void main(String[] args) {
+        System.out.println("hello, World!");
+    }
+}
