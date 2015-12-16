b204423fd0de7f34aea0068f51aa469be10f75fe
lokotochek <lokotochek@gmail.com>
2015-12-16 05:27:48 +0300

    Лактионов Александр, 497, TwitterStream+ModuleTests

---
+++ projects/alexlokotochek/src/main/java/TStream.java
@@ -0,0 +1,19 @@
+import runners.QuerySearchRunner;
+import runners.StreamRunner;
+import jcmdparser.Parser;
+import com.beust.jcommander.JCommander;
+
+public class TStream {
+
+    public static void main(String[] args) {
+
+        Parser jcp = new Parser();
+        new JCommander(jcp, args);
+
+        if (jcp.stream) {
+            StreamRunner.runStream(jcp);
+        } else {
+            QuerySearchRunner.runSearch(jcp);
+        }
+    }
+}
