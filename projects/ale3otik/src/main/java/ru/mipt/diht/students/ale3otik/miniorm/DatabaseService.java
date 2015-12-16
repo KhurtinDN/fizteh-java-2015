72f97cc86493ff551da226c3511746547139df13
Alex <aleksey.zotov@phystech.edu>
2015-12-16 19:51:36 +0300

    Database Service works properly

--- projects/ale3otik/src/main/java/ru/mipt/diht/students/ale3otik/miniorm/DatabaseService.java
+++ projects/ale3otik/src/main/java/ru/mipt/diht/students/ale3otik/miniorm/DatabaseService.java
@@ -4,9 +4,10 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 import java.lang.reflect.Field;
 import java.sql.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
 
-import static java.lang.Enum.valueOf;
 import static java.lang.annotation.ElementType.FIELD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -16,60 +17,103 @@ import static java.util.stream.Collectors.joining;
  * Created by alex on 15.12.15.
  */
 @SuppressWarnings("Duplicates")
-public class DatabaseService <T> {
+public class DatabaseService<T> {
     private Class<T> aClass;
     private Table annotation;
     private String table;
     private Field[] fields;
-    private Field primaryKeyField;
+    private int primaryKeyFieldId;
     private Connection connection;
-    private static String DATABASE_PROTOCOL_HEAD = "jdbc:h2:./database/miniorm";
-
+    private ArrayList<String> fieldNames;
+    private static final String PROTOCOL_HEAD = "jdbc:h2:./database/miniorm";
 
     @Target(TYPE)
     @Retention(RUNTIME)
     public @interface Table {
-        String name();
+        String name() default "";
     }
 
     @Target(FIELD)
     @Retention(RUNTIME)
     public @interface Column {
-        String name();
+        String name() default "";
     }
 
     @Target(FIELD)
     @Retention(RUNTIME)
-    public @interface PrimaryKey{}
+    public @interface PrimaryKey {
+    }
 
     private void validateClassType() throws DatabaseServiceException {
 
         int pkCount = 0;
-        for(Field f : fields) {
-            if(f.getAnnotatedType() == null) continue;
-            if(f.isAnnotationPresent(PrimaryKey.class) == true) {
-                ++pkCount;
-                primaryKeyField = f;
+        for (int i = 0; i < fields.length; ++i) {
+            if (fields[i].getAnnotatedType() == null) {
+                continue;
             }
-            if(pkCount > 1) {
+            if (fields[i].isAnnotationPresent(PrimaryKey.class)) {
+                if (!fields[i].isAnnotationPresent(Column.class)) {
+                    throw new DatabaseServiceException("@PrimaryKey must have @Column annotation");
+                }
+                ++pkCount;
+                primaryKeyFieldId = i;
+            }
+            if (pkCount > 1) {
                 throw new DatabaseServiceException("not unique @PrimaryKey field");
             }
-            if(H2DBTypeResolver.resolve(f.getType()) == null) {
-                throw new DatabaseServiceException(f.getType().toString() + "is not allowed");
+            if (H2DBTypeResolver.resolve(fields[i].getType()) == null) {
+                throw new DatabaseServiceException(fields[i].getType().toString() + "is not allowed");
             }
         }
-        if(pkCount == 0) {
+        if (pkCount == 0) {
             throw new DatabaseServiceException("@PrimaryKey wasn't found");
         }
     }
 
-    public DatabaseService(Class<T> dataClass) throws SQLException,DatabaseServiceException {
+    private void buildFieldNames() {
+        fieldNames = new ArrayList<>();
+        for (Field f : fields) {
+            Column column = f.getAnnotation(Column.class);
+            if (column == null) {
+                fieldNames.add(null);
+            } else {
+                if (column.name().length() > 0) {
+                    fieldNames.add(column.name());
+                } else {
+                    fieldNames.add(toSnakeCase(f.getName()));
+                }
+            }
+        }
+    }
+
+    private String toSnakeCase(String source) {
+        StringBuilder resultBuilder = new StringBuilder();
+        for (int i = 0; i < source.length(); ++i) {
+            if (source.charAt(i) >= 'A' && source.charAt(i) <= 'Z') {
+                if (i > 0 && source.charAt(i - 1) >= 'a' && source.charAt(i - 1) <= 'z') {
+                    resultBuilder.append("_");
+                }
+                resultBuilder.append((char) (source.charAt(i) - 'A' + 'a'));
+            } else {
+                resultBuilder.append(source.charAt(i));
+            }
+        }
+        return resultBuilder.toString();
+    }
+
+    public DatabaseService(Class<T> dataClass) throws SQLException, DatabaseServiceException {
         aClass = dataClass;
         annotation = aClass.getAnnotation(Table.class);
-        table = annotation.name();
+        if (annotation.name().length() > 0) {
+            table = annotation.name();
+        } else {
+            table = toSnakeCase(dataClass.getSimpleName());
+        }
+
         fields = aClass.getDeclaredFields();
-        connection = DriverManager.getConnection(DATABASE_PROTOCOL_HEAD);
         validateClassType();
+        buildFieldNames();
+        connection = DriverManager.getConnection(PROTOCOL_HEAD);
     }
 
     //    - возвращает запись по первичному ключу
@@ -77,26 +121,28 @@ public class DatabaseService <T> {
             IllegalAccessException, InstantiationException {
         List<T> result = new LinkedList<>();
 
-        while(rs.next()) {
+        while (rs.next()) {
             T instance = (T) aClass.newInstance();
-            for(Field field : fields) {
-                field.setAccessible(true);
-                Column column = field.getAnnotation(Column.class);
-                if(column == null) continue;
+            for (int i = 0; i < fields.length; ++i) {
+                fields[i].setAccessible(true);
+                String name = fieldNames.get(i);
+                if (name == null) {
+                    continue;
+                }
 
-                field.set(instance,rs.getObject(column.name()));
+                fields[i].set(instance, rs.getObject(name));
             }
             result.add(instance);
         }
         return result;
     }
 
-    public <T,K> T queryById(K key) throws SQLException,
+    public final <K> T queryById(K key) throws SQLException,
             DatabaseServiceException, IllegalAccessException, InstantiationException {
 
-        Column column = primaryKeyField.getAnnotation(Column.class);
+        String name = fieldNames.get(primaryKeyFieldId);
         PreparedStatement getStatement =
-                connection.prepareStatement("SELECT * FROM " + table + " WHERE " + column.name() + " = ?");
+                connection.prepareStatement("SELECT * FROM " + table + " WHERE " + name + " = ?");
         getStatement.setObject(1, key);
         ResultSet rs = getStatement.executeQuery();
         List<T> result = buildObjects(rs);
@@ -108,24 +154,23 @@ public class DatabaseService <T> {
     }
 
     // - возвращает все записи из таблицы
-    public <T> List<T> queryForAll() throws SQLException, IllegalAccessException, InstantiationException {
-        PreparedStatement getStatement= connection.prepareStatement("SELECT * FROM " + table);
+    public final <T> List<T> queryForAll() throws SQLException, IllegalAccessException, InstantiationException {
+        PreparedStatement getStatement = connection.prepareStatement("SELECT * FROM " + table);
         ResultSet rs = getStatement.executeQuery();
         return buildObjects(rs);
     }
 
     //- добавляет запись
-    public <T> void insert(T entity) throws SQLException, IllegalAccessException {
-
+    public final void insert(T entity) throws SQLException, IllegalAccessException {
         List<String> columns = new ArrayList<>();
         ArrayList<Object> values = new ArrayList<>();
 
-        for (Field field : fields) {
-            field.setAccessible(true);
-            Column column = field.getAnnotation(Column.class);
-            if (column != null) {
-                columns.add(column.name());
-                values.add(field.get(entity));
+        for (int i = 0; i < fields.length; ++i) {
+            fields[i].setAccessible(true);
+            String name = fieldNames.get(i);
+            if (name != null) {
+                columns.add(name);
+                values.add(fields[i].get(entity));
             }
         }
 
@@ -148,43 +193,60 @@ public class DatabaseService <T> {
     }
 
     // - редактирует запись по первичному ключу
-    public <T,K> void update(K key) throws SQLException {
-
+    public final void update(T newRecord) throws SQLException, IllegalAccessException {
+        if (deleteById(fields[primaryKeyFieldId].get(newRecord))) {
+            insert(newRecord);
+        }
     }
 
     //    удаляет запись по первичному ключу
-    public <T,K> void delete(K key) throws SQLException {
-
+    public final <K> boolean deleteById(K key) throws SQLException {
+        String name = fieldNames.get(primaryKeyFieldId);
+        PreparedStatement deleteStatement =
+                connection.prepareStatement("DELETE FROM " + table + " WHERE " + name + " = ?");
+        deleteStatement.setObject(1, key);
+        return deleteStatement.executeUpdate() > 0;
     }
 
-    // - создаёт таблицу по метаданным класса T. См. аннотации ниже.
-    public void createTable() throws SQLException {
+    public final boolean delete(T example) throws SQLException, IllegalAccessException {
+        return deleteById(fields[primaryKeyFieldId].get(example));
+    }
+
+    // - создаёт таблицу по метаданным класса T.
+    public final void createTable() throws SQLException {
         StringBuilder headBuilder = new StringBuilder();
-//        sBuilder.append(table).append(" (");
-        int i = 0;
-        for (Field field : fields) {
-            if(i == 0) {
-                ++i;
-            } else {
+        for (int i = 0; i < fields.length; ++i) {
+            if (i != 0) {
                 headBuilder.append(", ");
             }
-            Column column = field.getAnnotation(Column.class);
-            if (column != null) {
-                headBuilder.append(column.name()).append(" ")
-                        .append(H2DBTypeResolver.resolve(field.getType()));
-                if(field.isAnnotationPresent(PrimaryKey.class)) {
+            String name = fieldNames.get(i);
+            if (name != null) {
+                headBuilder.append(name).append(" ")
+                        .append(H2DBTypeResolver.resolve(fields[i].getType()));
+                if (i == primaryKeyFieldId) {
                     headBuilder.append(" NOT NULL PRIMARY KEY");
                 }
             }
         }
-
         connection.createStatement().execute("CREATE TABLE IF NOT EXISTS " + table
                 + "(" + headBuilder.toString() + ")");
     }
 
     // - удаляет таблицу, соответствующую T
-    public void dropTable() throws SQLException {
+    public final void dropTable() throws SQLException {
         Statement createStatement = connection.createStatement();
         createStatement.execute("DROP TABLE IF EXISTS " + table);
+    }
+
+    @Override
+    protected final void finalize() throws SQLException {
+        if (connection.isClosed()) {
+            return;
+        }
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            System.err.println("can't close connection: " + e.getMessage());
+        }
     }
 }
