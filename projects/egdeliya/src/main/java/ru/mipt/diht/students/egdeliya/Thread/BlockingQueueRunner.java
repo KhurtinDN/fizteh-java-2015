08729929c2d77c2d61d13a5c9b340cb1c60439a1
egdeliya <egdeliya@mail.ru>
2015-12-18 00:16:51 +0300

    BlockingQueue

---
+++ projects/egdeliya/src/main/java/ru/mipt/diht/students/egdeliya/Thread/BlockingQueueRunner.java
@@ -0,0 +1,11 @@
+package ru.mipt.diht.students.egdeliya.Thread;
+
+/**
+ * Created by Эгделия on 17.12.2015.
+ */
+public class BlockingQueueRunner {
+    public static void main(String[] args) {
+        BlockingQueue<String> queue = new BlockingQueue(Integer.parseInt(args[0]));
+        //queue.;
+    }
+}
