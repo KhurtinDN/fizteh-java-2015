b30e9eae71cd037a1ebbb5a460a6fbe4c1f2a43f
Semyon <semkozloff@gmail.com>
2015-12-17 20:51:53 +0300

    Finish Configuration test covering. ModuleTests is completely ready

--- projects/semyonkozloff/src/test/java/ru/mipt/diht/students/semyonkozloff/moduletests/library/ConfigurationTest.java
+++ projects/semyonkozloff/src/test/java/ru/mipt/diht/students/semyonkozloff/moduletests/library/ConfigurationTest.java
@@ -1,4 +1,81 @@
 package ru.mipt.diht.students.semyonkozloff.moduletests.library;
 
-public class ConfigurationTest {
+import com.beust.jcommander.JCommander;
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+@RunWith(DataProviderRunner.class)
+public class ConfigurationTest extends TestCase {
+
+    Configuration configuration = new Configuration();
+    JCommander jCommander = new JCommander(configuration);
+
+    @DataProvider
+    public static Object[][] argumentStringsDataProvider() {
+        return new Object[][] {
+                {
+                        "--help",
+                        null,
+                        null,
+                        false,
+                        false,
+                        16,
+                        true
+                },
+                {
+                        "-q \"python\" -s -r",
+                        "python",
+                        null,
+                        true,
+                        true,
+                        16,
+                        false
+                },
+                {
+                        "--query \"java\"",
+                        "java",
+                        null,
+                        false,
+                        false,
+                        16,
+                        false
+                },
+                {
+                        "-l 100 -q \"c++\" -r -p \"Moscow\"",
+                        "c++",
+                        "Moscow",
+                        false,
+                        true,
+                        100,
+                        false
+                }
+        };
+    }
+
+    @Test
+    @UseDataProvider("argumentStringsDataProvider")
+    public void testGetters(String argumentString,
+                            String expectedQuery,
+                            String expectedLocation,
+                            boolean expectedStreamFlag,
+                            boolean expectedRetweetsHidingFlag,
+                            int expectedLimit,
+                            boolean expectedHelpFlag) {
+        jCommander.parse(argumentString.split(" "));
+
+        assertThat(configuration.getQuery(), equalTo(expectedQuery));
+        assertThat(configuration.getLocation(), equalTo(expectedLocation));
+        assertThat(configuration.isStream(), equalTo(expectedStreamFlag));
+        assertThat(configuration.shouldHideRetweets(),
+                equalTo(expectedRetweetsHidingFlag));
+        assertThat(configuration.getLimit(), equalTo(expectedLimit));
+        assertThat(configuration.isHelp(), equalTo(expectedHelpFlag));
+    }
 }
