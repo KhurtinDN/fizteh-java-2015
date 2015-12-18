6ae697d4382e0f4cbec46dbc27d2eb3e0186aa38
feezboom <algebroid.2011@gmail.com>
2015-12-18 20:16:37 +0300

    MiniORM works

--- projects/feezboom/src/main/java/ru/mipt/diht/students/feezboom/MiniORM/DataBaseService.java
+++ projects/feezboom/src/main/java/ru/mipt/diht/students/feezboom/MiniORM/DataBaseService.java
@@ -11,6 +11,7 @@ import java.io.IOException;
 import java.lang.reflect.Field;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +26,7 @@ import java.util.List;
 // с сервером, и, видимо, Closable сам его закрывает
 // переопределённым методом (@Override) close
 public class DataBaseService<T> implements Closeable {
+    /* *****ПОЛЯ***** */
     // Поле нужное для соединения с сервером базы данных(H2)
     private JdbcConnectionPool connectionTool;
 
@@ -43,8 +45,9 @@ public class DataBaseService<T> implements Closeable {
     // Уникальный идентификатор элемента в базе данных
     private String primaryKeyFieldName;
     private int primaryKeyFieldNumber;
+    /* *****ПОЛЯ***** */
 
-
+    /* *******МЕТОДЫ******* */
     DataBaseService(Class<T> inputClass) throws Exception {
         // Это конструктор. Он вызывает инициализацию таблицы.
         tableInit(inputClass);
@@ -77,6 +80,7 @@ public class DataBaseService<T> implements Closeable {
         // Создавая этот список, будем одновременно проверять,
         // корректны ли входные данные - то есть все ли поля являются полями(поля - они же колонки в нашей таблице).
         int i = 0; // Это счётчик полей
+        boolean present = false; // Проверка на присутствие primaryKey
         for (Field ourColumn : fields) {
             // Если столбец не соответствует аннотации столбца
              if (!ourColumn.isAnnotationPresent(Column.class)) {
@@ -84,14 +88,14 @@ public class DataBaseService<T> implements Closeable {
              }
             // Получим имя столбца через его аннотацию
             String currentColumnName = ourColumn.getAnnotation(Column.class).name();
-            if (currentColumnName == "") {
+            if (currentColumnName.equals("")) {
                 currentColumnName = "EmptyName";
             }
 
             // Также проверить на то, что есть primary key, и он единственный
             // Если вдруг встретим поле с аннотацией PrimaryKey, то проверим,
-            // встречается оно в первый раз или нет
-            boolean present = false;
+            // встречается оно в первый раз или нет (переменная present)
+
             // Встретили поле с аннотацией PrimaryKey:
             if (ourColumn.isAnnotationPresent(PrimaryKey.class)) {
                 if (!present) {
@@ -233,20 +237,134 @@ public class DataBaseService<T> implements Closeable {
         }
     }
 
-    public void sendQuery(String querySQL) {
+    private void sendQuery(String querySQL) {
         try {
+            // Установим соединение для отправки запроса
             Connection connection = connectionTool.getConnection();
+            // И отправим его (execute)
             connection.createStatement().execute(querySQL);
         } catch (SQLException exception) {
-            System.err.println("Ошибка соединения SQL(отправка запроса).");
+            System.err.println("Ошибка SQL(отправка запроса).");
         }
     }
 
     public List<T> queryForAll() {
-        //todo
-        return new ArrayList<>();
+        // Возвращать будем список элементов таблицы
+        // Запрос: SELECT * FROM TABLE
+        String ourSQLQuery = "SELECT * FROM " + tableName;
+        // Список с результатами запроса
+        List<T> answerList = new ArrayList<>();
+        try {
+            // Устанавливаем соединение с сервером базы данных.
+            Connection connection = connectionTool.getConnection();
+            // Исполняем запрос. Получаем ответ.
+            // Получаем ответ на наш запрос в виде класса ResultSet
+            ResultSet baseResult = connection.createStatement().executeQuery(ourSQLQuery);
+
+            // Теперь нужно вытащить данные из ResultSet:
+            while (baseResult.next()) { // Пока есть что вытаскивать вытаскиваем.
+                // Создадим элемент, в который будем сувать данные
+                T ourTakenElement = ourTableClass.newInstance();
+                for (int i = 0; i < fields.length; i++) {
+                    // Из baseResult данные мы можем вытаскивать только по имени столбца
+                    // либо по номеру столбца, мы знаем имя(у нас есть массив имён полей),
+                    // и, к тому же, мы должны заранее знать тип вытаскиваемых данных.
+                    // Скажем, если класс fields[i] может быть присвоен Number'у, то вытащим его как Long
+                    if (fields[i].getClass().isAssignableFrom(Number.class)) {
+                        // Имя текущей колонки - fields[i]
+                        String currentColumnName = namesOfColumns[i];
+                        // Текущее значение, которое мы вытаскиваем из столбца с именем currentColumnName
+                        Long currentValue = baseResult.getLong(currentColumnName);
+                        // i-му полю нашего элемента присвоим вытащенное значение из ResultSet
+                        fields[i].set(ourTakenElement, currentValue);
+                    } else if (fields[i].getClass().isAssignableFrom(String.class)) {
+                        // Аналогично поступим со String
+                        String currentColumnName = namesOfColumns[i];
+                        String currentValue = baseResult.getString(currentColumnName);
+                        fields[i].set(ourTakenElement, currentValue);
+                    } else if (fields[i].getClass().isAssignableFrom(Object.class)) {
+                        // И аналогично поступим для Objects
+                        String currentColumnName = namesOfColumns[i];
+                        Object currentObject = baseResult.getObject(currentColumnName);
+                        fields[i].set(ourTakenElement, currentObject);
+                    }
+                }
+                answerList.add(ourTakenElement);
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Словили исключение SQL (queryForAll) :" + ex);
+        } catch (IllegalAccessException ex) {
+            System.out.println("Ошибка неправомерного доступа (queryForAll): " + ex);
+        } catch (InstantiationException ex) {
+            System.out.println("Ошибка создания экземпляра класса (queryForAll) : " + ex);
+        }
+
+        return answerList;
     }
 
+    public <K> T queryById(K primaryKey) throws Exception {
+        // Уникальные ключи элементов базы данных могут быть любого типа, поэтому мы делаем ещё один шаблон
+        // Запрос SELECT * FROM tableName WHERE ID = primaryKey
+        // Делается практически также так и queryForAll
+        String ourSQLQuery = "SELECT * FROM " + tableName + "WHERE " + primaryKeyFieldName + " = " + primaryKey;
+        // Здесь мы запрашиваем ровно один элемент из таблицы, по его уникальному ключу, поэтому
+        // тут не будет обработки результата в цикле, а просто единственный if
+        try {
+            // Устанавливаем соединение с сервером базы данных.
+            Connection connection = connectionTool.getConnection();
+            // Исполняем запрос. Получаем ответ.
+            // Получаем ответ на наш запрос в виде класса ResultSet
+            // Здесь он будет содержать единственный элемент, потому что мы запрашиваем
+            // по уникальному ключу, соответствующему единственному элементу
+            ResultSet baseResult = connection.createStatement().executeQuery(ourSQLQuery);
+
+            // Теперь нужно вытащить данные из ResultSet:
+            if (baseResult.next()) { // Пока есть что вытаскивать вытаскиваем.
+                // Создадим элемент, в который будем сувать данные
+                T ourTakenElement = ourTableClass.newInstance();
+                for (int i = 0; i < fields.length; i++) {
+                    // Из baseResult данные мы можем вытаскивать только по имени столбца
+                    // либо по номеру столбца, мы знаем имя.
+                    // и, к тому же, мы должны заранее знать тип вытаскиваемых данных
+                    // Скажем, что если класс fields[i] может быть присвоен Number'у, то вытащим его как Long
+                    if (fields[i].getClass().isAssignableFrom(Number.class)) {
+                        // Имя текущей колонки - fields[i]
+                        String currentColumnName = namesOfColumns[i];
+                        // Текущее значение, которое мы вытаскиваем из столбца с именем currentColumnName
+                        Long currentValue = baseResult.getLong(currentColumnName);
+                        // i-му полю нашего элемента присвоим вытащенное значение из ResultSet
+                        fields[i].set(ourTakenElement, currentValue);
+                    } else if (fields[i].getClass().isAssignableFrom(String.class)) {
+                        // Аналогично поступим со String
+                        String currentColumnName = namesOfColumns[i];
+                        String currentValue = baseResult.getString(currentColumnName);
+                        fields[i].set(ourTakenElement, currentValue);
+                    } else if (fields[i].getClass().isAssignableFrom(Object.class)) {
+                        // И аналогично поступим для Objects
+                        String currentColumnName = namesOfColumns[i];
+                        Object currentObject = baseResult.getObject(currentColumnName);
+                        fields[i].set(ourTakenElement, currentObject);
+                    }
+                }
+                // Вернём полученный результат
+                return ourTakenElement;
+            }
+
+        } catch (SQLException ex) {
+            System.out.println("Словили исключение SQL (queryById) :" + ex);
+        } catch (IllegalAccessException ex) {
+            System.out.println("Ошибка неправомерного доступа (queryById): " + ex);
+        } catch (InstantiationException ex) {
+            System.out.println("Ошибка создания экземпляра класса (queryById) : " + ex);
+        }
+        throw new Exception("Запрос по Id провалился (queryById).");
+    }
+
+    /* *******МЕТОДЫ******* */
+
+    // Переопределённый метод класса Closable, от которого мы унаследовались
+    // Закрывает соединение с базой данных.(сервером)
     @Override
     public void close() throws IOException {
         if (connectionTool != null) {
