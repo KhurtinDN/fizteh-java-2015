b591eaf5b54d1dbbb1aa08267fcc6f664361490e
Cheshear <Cheshear_nekorl@rambler.ru>
2015-12-19 05:22:14 +0900

    miniORMn

---
+++ solutions/popova/miniORMn/src/main/java/MiniORM/Table.java
@@ -0,0 +1,15 @@
+package MiniORM;
+
+/**
+ * Created by SM1 on 18.12.2015.
+ */
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Table {
+    String name() default "";
+}
