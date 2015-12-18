bafde3f153b296a0a0a2deaa4ff283b715398ae4
nick <username@hostname.name>
2015-12-18 19:16:30 +0300

    Some changes

---
+++ projects/nkarpachev/src/test/java/ru/mipt/diht/students/nkarpachev/twittertests/DateFormatToolsTest.java
@@ -0,0 +1,17 @@
+package ru.mipt.diht.students.nkarpachev.twittertests;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by kk on 12/16/15.
+ */
+public class DateFormatToolsTest extends TestCase {
+
+    public void testGetTweetDate() throws Exception {
+
+    }
+
+    public void testGetRetweetsCnt() throws Exception {
+
+    }
+}
