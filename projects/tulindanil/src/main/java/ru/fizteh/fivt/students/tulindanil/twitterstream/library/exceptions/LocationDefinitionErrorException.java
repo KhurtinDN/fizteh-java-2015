9c06f8d08fd78e37304b69630f5f2eba6e475d4a
tulindanil <tulindanil@gmail.com>
2015-12-19 13:53:45 +0300

    delete other projects

--- projects/tulindanil/src/main/java/ru/fizteh/fivt/students/tulindanil/twitterstream/library/exceptions/LocationDefinitionErrorException.java
+++
@@ -1,7 +0,0 @@
-package ru.fizteh.fivt.students.tulindanil.twitterstream.library.exceptions;
-
-public class LocationDefinitionErrorException extends Exception {
-    public LocationDefinitionErrorException(String message) {
-        super(message);
-    }
-}
