8a9969ecd6ce87d4ed1040ca19d38a23796c98c3
bulgakovad <kondaurova@phystech.edu>
2015-12-19 03:34:43 +0300

    Added ModuleTests

---
+++ TwitterModuleTests/src/main/java/ru.fizteh.fivt.students.bulgakova.TwitterStream/TwitterMainTest.java
@@ -0,0 +1,21 @@
+package ru.fizteh.fivt.students.bulgakova.TwitterStream;
+
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class TwitterMainTest {
+
+    @BeforeClass
+    public static void SetUpArguments() {}
+
+    @AfterClass
+    public static void CleanUpArguments() {}
+
+    @Test
+    public void WorkInStreamModeTest() {}
+
+    @Test
+     public void WorkInCommonModeTest() {}
+
+}
