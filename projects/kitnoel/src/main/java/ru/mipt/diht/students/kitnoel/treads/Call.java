e63fadfebaaa3d4da78d25f351f3bff1fee79c42
leonk <kitnoelka@gmail.com>
2015-12-19 08:54:21 +0300

    fix call

--- projects/kitnoel/src/main/java/ru/mipt/diht/students/kitnoel/treads/Call.java
+++ projects/kitnoel/src/main/java/ru/mipt/diht/students/kitnoel/treads/Call.java
@@ -4,76 +4,78 @@ package ru.mipt.diht.students.kitnoel.treads;
  * Created by leonk on 19.12.15.
  */
 import java.util.Random;
+import java.util.concurrent.BrokenBarrierException;
+import java.util.concurrent.CyclicBarrier;
 
 public class Call {
 
-    public static class Runner {
-        private static int curN = 0;
-        private static boolean finish = false;
-        private static  boolean alive = true;
-        private static Random random = new Random();
+    private static volatile boolean allReady;
+    private static boolean finish;
+    private static volatile Random chance = new Random();
+    private static CyclicBarrier beginAnswer;
+    private static CyclicBarrier endAnswer;
 
-        private static class ThreadClass extends Thread {
-            private volatile int number;
+    private static class Player extends Thread {
 
-            @Override
-            public void run() {
-                while (alive) {
-                    if (curN + 1 == number) {
-                        int x = random.nextInt(10);
-                        if (x < 1) {
-                            finish = false;
-                            System.out.println("No");
-                        } else {
-                            System.out.println("Yes");
-                        }
-                        curN++;
-                    } else {
-                        try {
-                            Thread.sleep(1000);
-                        } catch (InterruptedException ex) {
-                            ex.printStackTrace();
-                        }
-                    }
+        @Override
+        public void run() {
+            while (!finish) {
+                try {
+                    beginAnswer.await();
+                } catch (InterruptedException | BrokenBarrierException e) {
+                    e.printStackTrace();
                 }
-            }
-
-            ThreadClass(int num) {
-                this.number = num;
-            }
-        }
-
-        void run(int n) {
-            random = new Random();
-            curN = n;
-            ThreadClass[] threads = new ThreadClass[n];
-            for (int i = 0; i < n; i++) {
-                threads[i] = new ThreadClass(i + 1);
-                threads[i].start();
-            }
-            while (true) {
-                if (curN == n) {
-                    if (finish) break;
-                    finish = true;
-                    System.out.println("Are you ready?");
-                    curN = 0;
+                if (chance.nextInt(10) > 1) {
+                    System.out.println("Yes");
                 } else {
-                    try {
-                        Thread.sleep(1000);
-                    } catch (InterruptedException ex) {
-                        ex.printStackTrace();
-                    }
+                    System.out.println("No");
+                    allReady = false;
+                }
+                try {
+                    endAnswer.await();
+                } catch (InterruptedException | BrokenBarrierException ex) {
+                    ex.printStackTrace();
                 }
             }
-            alive = false;
         }
+
+
     }
 
     public static void main(String[] args) {
-        int n = 0;
-        if (args.length > 0) {
-            n = Integer.valueOf(args[0]);
+        if (args.length >= 1) {
+            finish = false;
+            int n = Integer.valueOf(args[0]);
+            beginAnswer = new CyclicBarrier(n + 1);
+            endAnswer = new CyclicBarrier(n + 1);
+            Player[] players = new Player[n];
+            for (int i = 0; i < n; i++) {
+                players[i] = new Player();
+                players[i].start();
+            }
+            finish = false;
+            while (!finish) {
+                System.out.println("Are you ready?");
+                allReady = true;
+                try {
+                    beginAnswer.await();
+                } catch (BrokenBarrierException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+                beginAnswer.reset();
+                try {
+                    endAnswer.await();
+                    if (allReady) {
+                        finish = true;
+                        for (int i = 0; i < n; i++) {
+                            players[i].join();
+                        }
+                        System.exit(0);
+                    }
+                } catch (BrokenBarrierException | InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         }
-        new Runner().run(n);
     }
 }
