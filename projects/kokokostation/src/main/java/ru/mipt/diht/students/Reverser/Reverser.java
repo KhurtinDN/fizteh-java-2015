b0a3d376eae13ba1b5c3b6c49450d3ac970afb20
Aleksandr Manakov <kokokostation@gmail.com>
2016-02-05 23:51:25 +0300

    one line solution

--- projects/kokokostation/src/main/java/ru/mipt/diht/students/Reverser/Reverser.java
+++ projects/kokokostation/src/main/java/ru/mipt/diht/students/Reverser/Reverser.java
@@ -3,13 +3,19 @@ package ru.mipt.diht.students.Reverser;
 import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 /**
  * Created by mikhail on 15.12.15.
  */
 public class Reverser {
     public static void main(String[] args) {
-        String[] numbers = StringUtils.join(args, " ").split("\\s+");
-        ArrayUtils.reverse(numbers);
-        System.out.println(StringUtils.join(numbers, " "));
+        Arrays.stream(Arrays.asList(args).stream().reduce((a, b) -> a + " " + b).get().split("\\s+"))
+                .collect(Collectors.toCollection(LinkedList::new))
+                .descendingIterator().forEachRemaining(a -> System.out.print(a + " "));
+        //p. s про то как ревёрсить не я придумал
     }
 }
