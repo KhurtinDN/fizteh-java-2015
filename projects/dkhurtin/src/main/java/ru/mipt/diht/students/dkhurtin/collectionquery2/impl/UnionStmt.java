c25a8bf98fdc57105c8efb5250c9e548a048b868
Andrew <algebroid.2011@gmail.com>
2016-07-01 19:50:58 +0300

    update twitterStreamer

--- projects/dkhurtin/src/main/java/ru/mipt/diht/students/dkhurtin/collectionquery2/impl/UnionStmt.java
+++
@@ -1,8 +0,0 @@
-package ru.mipt.diht.students.dkhurtin.collectionquery2.impl;
-
-public class UnionStmt {
-
-    public <T> FromStmt<T> from(Iterable<T> list) {
-        throw new UnsupportedOperationException();
-    }
-}
